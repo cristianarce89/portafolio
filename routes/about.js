410d2ead08642ce8573d6eb4afe158a9d55493f8
Cristian Arce <cristiancamilo_1989@hotmail.com>
2022-09-21 18:28:44 -0500

    arreglo del portafolio

--- routes/about.js
+++
@@ -1,7 +0,0 @@
-const express = require ('express');
-const aboutController = require("../controller/aboutController");
-const router = express.Router();
-
-router.get("/about", aboutController.about)
-
-module.exports = router;
