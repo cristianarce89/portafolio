ef2221ee5b46da3af8cc8f1699895ce67160ab81
Cristian Arce <cristiancamilo_1989@hotmail.com>
2022-09-20 20:00:01 -0500

    creacion de proyecto portafolio

---
+++ controller/aboutController.js
@@ -0,0 +1,9 @@
+const path = require("path");
+
+const aboutController = {
+    about: (req,res) => {
+        res.sendFile(path.join(__dirname, "../views/about.html"))
+    }
+}
+
+module.exports = aboutController;
